a514176f5cba92a547935c3feb59ad4fc9ae67cd
begonaOlea <bolea@programia.es>
2021-11-03 12:28:29 +0100

    escribir fichero

--- src/main/java/com/curso/ficheros/EjemploFicheros.java
+++ src/main/java/com/curso/ficheros/EjemploFicheros.java
@@ -4,42 +4,64 @@ import java.io.*;
 
 public class EjemploFicheros {
 
-	
 	public static void main(String[] args) throws IOException {
-		
-		
-		//crea un fichero
+
+		// crea un fichero
 		File fichero = new File("carta1.txt");
 		try {
 			boolean esNuevo = fichero.createNewFile();
-			if(esNuevo) {
+			if (esNuevo) {
 				System.out.println("He creado el fichero");
-			}else {
+			} else {
 				System.out.println("ya existe el fichero");
 			}
-			
+
 		} catch (IOException e) {
 			System.out.println("Error de escritura en disco");
 			e.printStackTrace();
 		}
-		
-		
-		//crear un diretorio
-		
+
+		// crear un diretorio
+
 		File directorio = new File("midirectorio");
 		boolean creado = directorio.mkdir();
 		System.out.println("¿he creado el directorio? " + creado);
-		
+
 		String[] contiene = directorio.list();
-		
-		//crea un fichero en el directorio
-		File f2 = new File(directorio,"carta3.txt");
+
+		// crea un fichero en el directorio
+		File f2 = new File(directorio, "carta3.txt");
 		f2.createNewFile();
+
+		// escribir
+
+		//try (PrintWriter salida = new PrintWriter(f2)) {
+		try (PrintWriter salida = 
+				new PrintWriter(new FileWriter(f2,true))) {
+		
+			salida.println("linea 1");
+			salida.println("linea 2");
+			salida.flush();
+			
+		}catch(IOException e){
+			System.out.println("No pudo escribir el fichero " + e.getMessage());
+		}
+		//finally {
+			// salida.close();
+		//}
+		
 		
 		//leer
-		//escribir
 		
+		BufferedReader entrada =
+				new BufferedReader(new FileReader(f2));
+		String linea = null;
+		do {
+			linea = entrada.readLine();
+			System.out.println(". " + linea);
+		}while(linea != null );
 		
-		
+		entrada.close();
+
 	}
 }
