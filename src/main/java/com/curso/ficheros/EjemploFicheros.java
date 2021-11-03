4972a145c65453f99e170dc1db250b53773583ac
begonaOlea <bolea@programia.es>
2021-11-03 12:04:42 +0100

    FILE

--- src/main/java/com/curso/ficheros/EjemploFicheros.java
+++ src/main/java/com/curso/ficheros/EjemploFicheros.java
@@ -1,5 +1,45 @@
 package com.curso.ficheros;
 
+import java.io.*;
+
 public class EjemploFicheros {
 
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		//crea un fichero
+		File fichero = new File("carta1.txt");
+		try {
+			boolean esNuevo = fichero.createNewFile();
+			if(esNuevo) {
+				System.out.println("He creado el fichero");
+			}else {
+				System.out.println("ya existe el fichero");
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error de escritura en disco");
+			e.printStackTrace();
+		}
+		
+		
+		//crear un diretorio
+		
+		File directorio = new File("midirectorio");
+		boolean creado = directorio.mkdir();
+		System.out.println("¿he creado el directorio? " + creado);
+		
+		String[] contiene = directorio.list();
+		
+		//crea un fichero en el directorio
+		File f2 = new File(directorio,"carta3.txt");
+		f2.createNewFile();
+		
+		//leer
+		//escribir
+		
+		
+		
+	}
 }
